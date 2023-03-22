a8c0a24f0bec42395999af009a354d764be1a0f5
eduard <ed.khusnutdinow@yandex.ru>
2023-03-23 00:21:17 +0700

    Defined flask application with run mode Made up validation for env variables

---
+++ application/controller/app.py
@@ -0,0 +1,70 @@
+import os
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask
+import logging
+
+from application.config import DEFAULT_PORT, DEV_RUN_MODE, PROD_RUN_MODE, APPLICATION_ROOT, LOGING_PATH, HOST
+
+
+def check_env_vars():
+    """Check all environment variables"""
+
+    global app
+    global run_mode
+    global port
+
+    # Get run mode
+    run_mode = os.environ.get('run_mode', DEV_RUN_MODE)
+    message = f'Run mode {run_mode}'
+    app.logger.info(message)
+
+    # Get port
+    port = os.environ.get('port', DEFAULT_PORT)
+    if port == DEFAULT_PORT:
+        if run_mode == DEV_RUN_MODE:
+            app.logger.info(f'Application port is not set. Using default {port}')
+        elif run_mode == PROD_RUN_MODE:  # Do not run in production with default port
+            app.logger.info(f'Set up application port')
+            raise KeyError(message)
+    else:
+        app.logger.info(f'Using port {port}')
+
+
+def init_logging():
+    """Initialise app logging"""
+
+    global app
+    global logging_path
+
+    # Create logging file. Clean up if exists
+    logfile = open(logging_path, 'w')
+    logfile.close()
+
+    # Define formatting pattern
+    formatter = logging.Formatter(
+        fmt='>>> %(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+    handler = RotatingFileHandler(logging_path)
+    handler.setFormatter(formatter)
+
+    default_logger = logging.getLogger()
+    default_logger.setLevel(logging.INFO)
+
+    app.logger.addHandler(handler)
+
+
+run_mode = DEV_RUN_MODE
+host = HOST
+port = DEFAULT_PORT
+logging_path = LOGING_PATH
+
+app = Flask(__name__)
+app.config['APPLICATION_ROOT'] = APPLICATION_ROOT
+
+init_logging()
+check_env_vars()
+
+# import application.controller
