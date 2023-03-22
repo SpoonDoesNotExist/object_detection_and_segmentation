790a399ae841b5b44cb8510b1428f1b24dd92323
eduard <ed.khusnutdinow@yandex.ru>
2023-03-23 00:25:38 +0700

    Defined cli

---
+++ application/model/cli.py
@@ -0,0 +1,27 @@
+import typer as typer
+
+from application.model.model import MyDetectionSegmentationModel
+
+det_seg_model = MyDetectionSegmentationModel()
+det_seg_model.warmup()
+
+cli_app = typer.Typer()
+
+
+@cli_app.command()
+def demo():
+    raise NotImplementedError()
+
+
+@cli_app.command()
+def train(dataset):
+    raise NotImplementedError()
+
+
+@cli_app.command()
+def evaluate(dataset):
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    cli_app()
