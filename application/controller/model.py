c606836824494726a2c9c822a522fba3052f35ad
eduard <ed.khusnutdinow@yandex.ru>
2023-04-22 23:42:33 +0700

    Added docstrings

--- application/controller/model.py
+++ application/controller/model.py
@@ -13,11 +13,21 @@ model_service = ModelService(app.logger)
 
 @model_page.route('/index')
 def index():
+    """
+    Renders the upload page template for the model page.
+    """
     return render_template(UPLOAD_TEMPLATE_NAME)
 
 
 @model_page.route('/demo/<filename>')
 def demo(filename):
+    """
+    Renders the video demo page template for a specified file.
+
+    :param filename: The filename of the video to be displayed.
+
+    :return: The rendered video demo page template.
+    """
     file_path = f'{app.static_folder}/{filename}'
     if not os.path.exists(file_path):
         print('aborting')
@@ -25,10 +35,15 @@ def demo(filename):
 
     return render_template(VIDEO_TEMPLATE_NAME, filename=filename)
 
-
+# ----------------------------------------------------------------------------------------------------------------
 # Next endpoints are support. Not for user
 @model_page.route('/upload', methods=['POST'])
 def upload():
+    """
+    Handles uploading of a file to be used in the demo.
+
+    :return: Redirect to the demo page for the uploaded file.
+    """
     file = request.files.get('file')
 
     if file is None:
@@ -45,6 +60,13 @@ def upload():
 
 @model_page.route('/demo/<filename>/video_feed')
 def video_feed(filename):
+    """
+    Returns a video feed for a specified file.
+
+    :param filename: The filename of the video to be displayed.
+
+    :return: A video feed of the specified file.
+    """
     return Response(
         model_service.demo(f'{app.static_folder}/{filename}'),
         mimetype=VIDEO_FEED_MIMETYPE
@@ -53,5 +75,12 @@ def video_feed(filename):
 
 @model_page.errorhandler(404)
 def not_found_error(e):
+    """
+    Handles a 404 error on the model page.
+
+    :param e: The exception that was raised.
+
+    :return: The rendered error page template.
+    """
     app.logger.info(str(e))
     return render_template(ERROR_TEMPLATE_NAME, text=str(e))
