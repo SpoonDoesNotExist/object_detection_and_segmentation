acd2a77575a9042a855e8978cd27a6f66a531edf
eduard <ed.khusnutdinow@yandex.ru>
2023-04-22 23:41:03 +0700

    Added docstrings

--- application/controller/info.py
+++ application/controller/info.py
@@ -10,4 +10,9 @@ info_service = InfoService(app.logger)
 
 @info_page.route('/info', methods=['GET'])
 def info():
+    """
+    Endpoint to retrieve credentials information.
+
+    :return: A JSON object with the credentials information.
+    """
     return jsonify(info_service.get_credentials())
