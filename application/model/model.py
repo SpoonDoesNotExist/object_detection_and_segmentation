7a69af3ebdbc34d5a4840129abec692b0800d67c
eduard <ed.khusnutdinow@yandex.ru>
2023-04-22 23:43:17 +0700

    Added docstrings. Added process, evaluate, train logic.

--- application/model/model.py
+++ application/model/model.py
@@ -3,67 +3,116 @@ import logging
 import cv2
 import numpy as np
 import torch
-
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
 from config import MODEL_PATH
-
-from . import arcitecture
+from .arcitecture import UNet
 from .base_model import BaseModel
+from .dataset import CloddDataset
 
 
 class MyDetectionSegmentationModel(BaseModel):
+    """
+    A custom detection and segmentation model for identifying contours in images using a UNet architecture.
+    """
 
-    def __init__(self, logger=logging.getLogger()):
+    def __init__(self, mask_threshold, logger=logging.getLogger()):
+        """
+
+        :param mask_threshold:  A threshold value for binarizing the output mask.
+        :param logger: A logger object for logging messages.
+        """
         super().__init__()
-        self.model = arcitecture.PetModel("unetplusplus", "efficientnet-b0", in_channels=3, out_classes=1)
+        self.mask_threshold = mask_threshold
+        self.model = UNet(n_channels=3, n_classes=1, out_channels=8)
         self.logger = logger
 
     def process(self, frame):
-        if self.model is None:
-            raise Exception('Load model!')
+        """
+        Process a single frame of an image.
 
+        :param frame: A single frame of an image.
+
+        :return: A tuple of the processed frame and a list of contour arrays.
+        """
         frame = self.__preprocess_frame(frame)
         with torch.no_grad():
-            probs = self.model(frame).sigmoid()
+            input_data = torch.from_numpy(frame.transpose(-1, 0, 1)).unsqueeze(0).type(torch.float32)
+            probs = self.model(input_data).sigmoid()
 
-        frame, mask = frame.numpy().squeeze().transpose(1, 2, 0), probs.numpy().squeeze() > 0.5
+        mask = probs.numpy().squeeze() > self.mask_threshold
         contours = self.__get_contours(mask)
         return frame, contours
 
     def warmup(self):
-        self.model.load_state_dict(torch.load(MODEL_PATH))
+        """
+        Load the model from a saved file and prepare it for inference
+
+        :return: None
+        """
+        self.model = torch.jit.load(MODEL_PATH)
         self.model.eval()
 
-    def evaluate(self, dataset):
-        pass
+    def evaluate(self, dataset_path):
+        """
+        Evaluate the model on a dataset.
 
-    def train(self, dataset):
-        pass
+        :param dataset_path: The dataset path to evaluate the model on.
+        :return: None
+        """
+        cds = CloddDataset(dataset_path / 'result.json', ['big_clod'])
+
+        valid_dataloader = DataLoader(cds, batch_size=16, shuffle=False)
+
+        trainer = pl.Trainer(gpus=1, max_epochs=30, log_every_n_steps=10)
+        trainer.test(self.model, valid_dataloader)
+
+    def train(self, dataset_path):
+        """
+        Train the model on a dataset.
+
+        :param dataset_path: The dataset path to train the model on.
+
+        :return: None
+        """
+        cds = CloddDataset(dataset_path / 'result.json', ['big_clod'])
+
+        train_dataset = torch.utils.data.Subset(cds, range(140))
+        valid_dataset = torch.utils.data.Subset(cds, range(140, 140 + 42))
+
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=16,
+            shuffle=True,
+        )
+        valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False)
+
+        trainer = pl.Trainer(gpus=1, max_epochs=30, log_every_n_steps=10)
+        trainer.fit(self.model, train_dataloader, valid_dataloader)
 
     def __get_contours(self, segmentation_mask):
+        """
+        Extract the contours from a binary segmentation mask.
+
+        :param segmentation_mask: A binary segmentation mask.
+
+        :return: A list of contour arrays.
+        """
         segmentation_mask = np.uint8(segmentation_mask)
         contours, _ = cv2.findContours(segmentation_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         return contours
 
     def __preprocess_frame(self, frame):
-        frame = self.__crop_to_correct_shape(frame)
-        # brightness
+        """
+        Preprocess a single frame.
 
+        :param frame: A single frame to preprocess.
+
+        :return: Preprocessed frame.
+        """
+        frame = cv2.convertScaleAbs(frame, alpha=3, beta=0)
+        frame = cv2.resize(
+            frame,
+            (frame.shape[1] // 2, frame.shape[0] // 2)
+        )
         return frame
-
-    def __crop_to_correct_shape(self, frame):
-        height, width, channels = frame.shape
-        h_diff, w_diff = height % 32, width % 32
-        cropped_frame = frame
-
-        if h_diff != 0:
-            left_diff, right_diff = self.__get_crop_size(h_diff)
-            cropped_frame = cropped_frame[left_diff:-right_diff, :, :]
-        if w_diff != 0:
-            left_diff, right_diff = self.__get_crop_size(w_diff)
-            cropped_frame = cropped_frame[:, left_diff:-right_diff, :]
-
-        cropped_frame = cropped_frame.transpose(-1, 0, 1)
-        return torch.from_numpy(cropped_frame).unsqueeze(0)
-
-    def __get_crop_size(self, diff):
-        return int(np.ceil(diff / 2.)), int(np.floor(diff / 2.))
