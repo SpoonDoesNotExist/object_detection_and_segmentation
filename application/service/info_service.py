bb7e9ec65637a0be119afb6e16606e1a91ce16eb
eduard <ed.khusnutdinow@yandex.ru>
2023-04-22 23:41:14 +0700

    Added docstrings

--- application/service/info_service.py
+++ application/service/info_service.py
@@ -3,9 +3,15 @@ from config import DOCKER_BUILD_DATETIME, TRAIN_METRICS_PATH, TEST_METRICS_PATH
 
 class InfoService:
     def __init__(self, logger):
+        """
+        :param logger:  An instance of a logger object to log messages.
+        """
         self.logger = logger
 
     def get_credentials(self):
+        """
+        Returns a dictionary with the author's credentials, Docker build datetime, and model metrics.
+        """
         docker_datetime = self._read_data(DOCKER_BUILD_DATETIME)
         train_metrics = self._read_data(TRAIN_METRICS_PATH)
         test_metrics = self._read_data(TEST_METRICS_PATH)
@@ -23,6 +29,13 @@ class InfoService:
         }
 
     def _read_data(self, path):
+        """
+        Reads data from the specified file.
+
+        :param path: The path to the file to read.
+
+        :return: The contents of the file as a string.
+        """
         with open(path, 'r') as f:
             return ' '.join(
                 f.readlines()
