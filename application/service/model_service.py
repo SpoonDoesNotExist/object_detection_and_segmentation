34488297f9d8b41cdd25bb73e7ca486927a19de5
eduard <ed.khusnutdinow@yandex.ru>
2023-04-22 23:44:01 +0700

    Added docstrings.

--- application/service/model_service.py
+++ application/service/model_service.py
@@ -3,17 +3,21 @@ import logging
 import cv2
 import numpy as np
 
-from config import TRAIN_METRICS_PATH, TEST_METRICS_PATH, DRAW_COLOR, DRAW_COLOR_MAX, CONTOUR_AREA_THRESHOLD
+from config import TRAIN_METRICS_PATH, TEST_METRICS_PATH, DRAW_COLOR, DRAW_COLOR_MAX, CONTOUR_AREA_THRESHOLD, \
+    MASK_THRESHOLD
 from model.model import MyDetectionSegmentationModel
 
 
 class ModelService:
     def __init__(self, logger=None):
+        """
+        :param logger: A logging object to use for logging, defaults to None
+        """
         self.logger = logger
         if self.logger is None:
             self.logger = self.__create_logger('default_logger', logging.DEBUG)
 
-        self.model = MyDetectionSegmentationModel(self.logger)
+        self.model = MyDetectionSegmentationModel(MASK_THRESHOLD, self.logger)
         self.model.warmup()
 
         self.color = DRAW_COLOR
@@ -58,17 +62,22 @@ class ModelService:
             yield self.__get_jpg_bytes(frame)
 
     def demo_cli(self, file_path):
-        cap = cv2.VideoCapture(file_path)
-        frame_area = None
+        """
+        Open video file and stream it as application.
 
+        :param file_path: The path to the video file to open
+
+        :return: None
+        """
+        cap = cv2.VideoCapture(file_path)
+
+        frame_area = None
         while cap.isOpened():
             success, frame = cap.read()
 
-            frame = cv2.convertScaleAbs(frame, alpha=3, beta=0)
-            frame = cv2.resize(
-                frame,
-                (frame.shape[1] // 2, frame.shape[0] // 2)
-            )
+            # Press Q on keyboard to  exit
+            if (cv2.waitKey(25) & 0xFF == ord('q')) or not success:
+                break
 
             # Process the frame using model
             frame, clod_contours = self.model.process(frame)
@@ -80,27 +89,48 @@ class ModelService:
                 areas = self.__calc_areas(clod_contours, frame_area)
                 clod_contours = self.__filter_contours(clod_contours, areas)
 
-                self.__draw_contours(frame, clod_contours, DRAW_COLOR)
+                self.__draw_contours(frame, clod_contours, DRAW_COLOR, -1)
                 self.__draw_max_contour(frame, clod_contours)
             else:
                 print(f'No clodds found')
 
             cv2.imshow('Frame', frame)
 
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
+    def __draw_contours(self, frame, contours, color, thickness=1):
+        """
+        Draw contours on the given frame with the specified color.
 
-    def __draw_contours(self, frame, contours, color):
-        cv2.drawContours(frame, contours, -1, color, 1)
+        :param frame: The frame on which to draw the contours
+        :param contours: The list of contours to draw
+        :param color: The color with which to draw the contours
+
+        :return: None
+        """
+        cv2.drawContours(frame, contours, -1, color, thickness)
 
     def __calc_areas(self, contours, full_area):
+        """
+        Calculate the areas of the given contours.
+
+        :param contours: The list of contours whose areas to calculate
+        :param full_area: The full area of the frame
+
+        :return: A numpy array containing the areas of the contours
+        """
         return np.fromiter(map(
             cv2.contourArea,
             contours
         ), dtype=np.float64) / full_area
 
     def __draw_max_contour(self, frame, contours):
+        """
+        Draw the maximum contour on the given frame.
+
+        :param frame: The frame on which to draw the maximum contour
+        :param contours:  The list of contours from which to find the maximum contour
+
+        :return: None
+        """
         frame_area = cv2.countNonZero(np.ones(frame.shape[:2]))
 
         areas = self.__calc_areas(contours, frame_area)
@@ -109,24 +139,45 @@ class ModelService:
         max_idx = np.argmax(areas)
         max_area = areas[max_idx]
 
-        self.__add_text(frame, f'Max area: {max_area}')
-        self.__draw_contours(frame, [contours[max_idx]], DRAW_COLOR_MAX)
+        self.__add_text(frame, f'Max area: {max_area:.5f}')
+        self.__add_text(frame, f"Clods count: {len(contours)}", 40, 20)
+        self.__draw_contours(frame, [contours[max_idx]], DRAW_COLOR_MAX, -1)
 
     def __get_jpg_bytes(self, frame):
-        # Convert the frame to a JPEG-encoded byte string
+        """
+        Convert the given frame to a JPEG-encoded byte string.
+
+        :param frame: The frame to convert to a JPEG-encoded byte string
+
+        :return: The JPEG-encoded byte string
+        """
         processed_frame = cv2.imencode('.jpg', frame)[1].tobytes()
-        # Returns the JPEG-encoded frame as a MIME multipart message with type "image/jpeg"
         return b'--frame\r\n' + b'Content-Type: image/jpeg\r\n\r\n' + processed_frame + b'\r\n'
 
     def __save_metrics(self, train_metrics, test_metrics):
+        """
+        Save the training and test metrics to file.
+
+        :param train_metrics: The training metrics to save
+        :param test_metrics: The test metrics to save
+
+        :return: None
+        """
         with open(TRAIN_METRICS_PATH, 'w') as f:
             f.write(str(train_metrics))
         with open(TEST_METRICS_PATH, 'w') as f:
             f.write(str(test_metrics))
 
-    def __add_text(self, img, text):
+    def __add_text(self, img, text, top_margin=10, right_margin=20):
         """
         Add text to the top right corner of an image.
+
+        :param img: The image to which to add text
+        :param text: The text to add to the image
+        :param top_margin: The margin from the top of the frame
+        :param right_margin: The margin from the right of the frame
+
+        :return: The image with the text added
         """
         # Set font parameters
         font = cv2.FONT_HERSHEY_SIMPLEX
@@ -137,8 +188,8 @@ class ModelService:
         text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
 
         # Set text position
-        text_x = img.shape[1] - text_size[0] - 10  # 10 pixels from right edge
-        text_y = text_size[1] + 10  # 10 pixels from top edge
+        text_x = img.shape[1] - text_size[0] - right_margin  # 10 pixels from right edge
+        text_y = text_size[1] + top_margin  # 10 pixels from top edge
 
         # Add text to image
         cv2.putText(img, text, (text_x, text_y), font, font_scale, (0, 0, 255), font_thickness, cv2.LINE_AA)
@@ -146,6 +197,14 @@ class ModelService:
         return img
 
     def __filter_contours(self, contours, areas):
+        """
+        Filter the given contours by area.
+
+        :param contours: The list of contours to filter
+        :param areas: The areas of the contours
+
+        :return: The filtered list of contours
+        """
         indices = [i for i, x in enumerate(areas) if x >= self.contour_area_threshold]
         if len(indices) == 0:
             return contours
@@ -153,7 +212,12 @@ class ModelService:
 
     def __create_logger(self, name, level=logging.INFO):
         """
-        Create a new logger with the specified name and log level.
+        Create a logger object.
+
+        :param name: The name of the logger
+        :param level: The logging level, defaults to logging.INFO
+
+        :return: The logger object
         """
         logger = logging.getLogger(name)
         logger.setLevel(level)
